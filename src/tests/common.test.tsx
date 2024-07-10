e2906e8961ad6ff3f87b7040fbbebae226864fa8
Deba0099 <debabrata.dash@dazn.com>
2024-07-11 01:02:22 +0530

    License added

--- src/tests/common.test.tsx
+++ src/tests/common.test.tsx
@@ -1,8 +1,7 @@
 import { render } from '@testing-library/react'
 
 import 'jest-canvas-mock'
-
-import { App } from '../../src'
+import { App } from '..'
 
 describe('Common render', () => {
   it('renders without crashing', () => {
